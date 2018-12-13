7e94a1596e0c039f1f9b16f5674bcea76781e3c2
Samit Nakarmi <samit.wizard@gmail.com>
2018-12-13 14:02:27 +0545

    enemies added

--- main.js
+++ main.js
@@ -4,6 +4,26 @@ var hero = {
 };
 
 var missiles = [];
+var enemies = [
+  { left: 200, top: 100 },
+  { left: 300, top: 100 },
+  { left: 400, top: 100 },
+  { left: 500, top: 100 },
+  { left: 600, top: 100 },
+  { left: 700, top: 100 },
+  { left: 800, top: 100 },
+  { left: 900, top: 100 },
+  { left: 200, top: 175 },
+  { left: 300, top: 175 },
+  { left: 400, top: 175 },
+  { left: 500, top: 175 },
+  { left: 600, top: 175 },
+  { left: 700, top: 175 },
+  { left: 800, top: 175 },
+  { left: 900, top: 175 },
+];
+
+var snd = new Audio('assets/gameSounds/shoot.wav');
 
 document.onkeydown = function(e) {
   console.log(e);
@@ -16,16 +36,16 @@ document.onkeydown = function(e) {
     // console.log('right is pressed');
     hero.left = hero.left + 10;
     moveHero();
-  }
-  // else if (e.keyCode === 38) {
-  //   hero.top = hero.top - 10;
-  //   moveHero();
-  // } else if (e.keyCode === 40) {
-  //   hero.top = hero.top + 10;
-  //   moveHero();
-  // }
-  else if (e.keyCode === 32) {
+  } else if (e.keyCode === 38) {
+    hero.top = hero.top - 10;
+    moveHero();
+  } else if (e.keyCode === 40) {
+    hero.top = hero.top + 10;
+    moveHero();
+  } else if (e.keyCode === 32) {
     console.log('FIRE');
+
+    snd.play();
 
     missiles.push({
       left: hero.left + 15,
@@ -37,7 +57,7 @@ document.onkeydown = function(e) {
 
 function moveHero() {
   document.getElementById('hero').style.left = hero.left + 'px';
-  // document.getElementById('hero').style.top = hero.top + 'px';
+  document.getElementById('hero').style.top = hero.top + 'px';
 }
 
 function drawMissiles() {
@@ -59,9 +79,20 @@ function moveMissiles() {
   }
 }
 
+function drawEnemies() {
+  document.getElementById('enemies').innerHTML = '';
+  for (var enemy = 0; enemy < enemies.length; enemy++) {
+    document.getElementById(
+      'enemies',
+    ).innerHTML += `<div class = 'enemy' style = 'left:${enemies[enemy].left}px;
+       top:${enemies[enemy].top}px;'></div>`;
+  }
+}
+
 function gameLoop() {
   setTimeout(gameLoop, 100);
   moveMissiles();
   drawMissiles();
+  drawEnemies();
 }
 gameLoop();
