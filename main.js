fd52eb005330a15bbc790e2bc9dfaaaa918522e2
Samit Nakarmi <samit.wizard@gmail.com>
2018-12-21 10:44:28 +0545

    enemy sideways, introscreen

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
+var difficulty;
+
 class Game {
-  constructor() {
+  constructor(difficulty) {
     this.heroHasDied = false;
     this.snd = new Audio('assets/gameSounds/shoot.wav');
     this.snd2 = new Audio('assets/gameSounds/explosion.wav');
-    this.heroHasDied = false;
     this.MAX_HEIGHT = 800;
     this.MAX_WIDTH = document.getElementById('background').offsetWidth;
     this.ref = null;
     this.enemyCount = 0;
-
+    this.difficulty = difficulty;
     this.enemies = [];
+    this.bool = false;
     // this.enemies = [
     //   new Enemy(200, 100),
     //   { left: 300, top: 100 },
@@ -41,14 +43,21 @@ class Game {
     heroElement.style.top = '700px';
     this.missiles = [];
 
+    var enemyElement = document.getElementById('enemy');
+
     //-------------Keys Pressed----------------
     document.onkeydown = (e) => {
       if (!this.heroHasDied) {
         if (e.keyCode === 37 && this.hero.left >= 50) {
           this.hero.left = this.hero.left - 10;
+          //sprite left here
+          heroElement.style.backgroundPosition = '0px 0px';
           this.hero.moveHero();
         } else if (e.keyCode === 39 && this.hero.left <= this.MAX_WIDTH - 50) {
           this.hero.left = this.hero.left + 10;
+          //sprite right here
+          heroElement.style.backgroundPosition = '-328px 0px';
+
           this.hero.moveHero();
         } else if (e.keyCode === 38 && this.hero.top >= 50) {
           this.hero.top = this.hero.top - 10;
@@ -69,10 +78,13 @@ class Game {
         }
       }
     };
+    document.onkeyup = (e) => {
+      heroElement.style.backgroundPosition = '-165px 0px';
+    };
   }
 
   drawEnemies() {
-    console.log(document);
+    // console.log(document);
     document.getElementById('enemies').innerHTML = '';
     for (var enemy = 0; enemy < this.enemies.length; enemy++) {
       document.getElementById('enemies').innerHTML += `<div class = 'enemy' style = 'left:${this.enemies[enemy].left}px;
@@ -82,7 +94,29 @@ class Game {
 
   moveEnemies() {
     for (var enemy = 0; enemy < this.enemies.length; enemy++) {
-      this.enemies[enemy].top = this.enemies[enemy].top + 1;
+      if (this.bool) {
+
+        this.enemies[enemy].left = this.enemies[enemy].left + (1 + this.difficulty);
+        if (this.enemies[enemy].left >= this.MAX_WIDTH - 80) {
+          this.bool = !this.bool;
+          for (var i = 0; i < this.enemies.length; i++) {
+            this.enemies[i].top = this.enemies[i].top + (25 + this.difficulty);
+          }
+        }
+      } else {
+        this.enemies[enemy].left = this.enemies[enemy].left - 2 * (1 + this.difficulty);
+        if (this.enemies[enemy].left <= 80) {
+          this.bool = !this.bool;
+          for (var i = 0; i < this.enemies.length; i++) {
+            this.enemies[i].top = this.enemies[i].top + (25 + this.difficulty);
+          }
+        }
+      }
+
+      if (this.enemies[enemy].top >= 745) {
+        this.heroHasDied = true;
+        this.gameOver();
+      }
     }
   }
 
@@ -99,6 +133,10 @@ class Game {
       this.missiles[missile].top = this.missiles[missile].top - 15;
     }
   }
+
+  // moveEnemySprite() {
+  //   enemyElement.style.backgroundPosition = '-328px 0px';
+  // }
 
   collisionDetection() {
     for (var enemy = 0; enemy < this.enemies.length; enemy++) {
@@ -127,13 +165,8 @@ class Game {
         this.hero.left >= this.enemies[x].left
       ) {
         this.heroHasDied = true;
+        this.gameOver();
         // console.log('heroHit');
-
-        var body = document.getElementById('hero');
-        body.style.backgroundImage = "url('assets/heroDie.jpg')";
-        clearInterval(this.ref);
-        var x = document.getElementById('myDialog');
-        x.show();
       }
     }
   }
@@ -145,7 +178,7 @@ class Game {
   }
 
   play() {
-    console.log('play function');
+    // console.log('play function');
     this.moveMissiles();
     this.drawMissiles();
     this.moveEnemies();
@@ -155,12 +188,14 @@ class Game {
 
     this.collisionDetection();
     this.heroCollision();
+
     if (this.enemies.length === 0) {
-      for (var left = 200; left <= 1000; left = left + 100) {
-        this.enemies.push(new Enemy(left, 100));
-        this.enemies.push(new Enemy(left, 175));
-        this.enemyCount = this.enemyCount + 2;
-      }
+      alert('You win');
+      // for (var left = 200; left <= 1000; left = left + 100) {
+      //   this.enemies.push(new Enemy(left, 100));
+      //   this.enemies.push(new Enemy(left, 175));
+      //   this.enemyCount = this.enemyCount + 2;
+      // }
     }
   }
 
@@ -169,6 +204,14 @@ class Game {
     this.ref = setInterval(() => {
       self.play();
     }, 100);
+  }
+
+  gameOver() {
+    var body = document.getElementById('hero');
+    body.style.backgroundImage = "url('assets/heroDie.jpg')";
+    clearInterval(this.ref);
+    var x = document.getElementById('myDialog');
+    x.show();
   }
 }
 
@@ -193,8 +236,9 @@ class Enemy {
 
 var game;
 
-function gameLoop() {
-  game = new Game();
+function gameLoop(difficulty) {
+  var x = difficulty;
+  game = new Game(x);
   game.gameloop();
   // setInterval(() => {
   //   game.play();
@@ -202,15 +246,40 @@ function gameLoop() {
 }
 
 function replay() {
-  console.log('replay called');
+  // console.log('replay called');
 
   var x = document.getElementById('myDialog');
   x.close();
 
   var body = document.getElementById('hero');
-  body.style.backgroundImage = "url('assets/hero.png')";
+  body.style.backgroundImage = "url('assets/airplane.png')";
 
-  gameLoop();
+  gameLoop(difficulty);
 }
 
-gameLoop();
+function start(level) {
+  console.log(level.innerText);
+
+  var lvl = level.innerText;
+  switch (lvl) {
+    case 'Easy':
+      difficulty = 0;
+      break;
+    case 'Medium':
+      difficulty = 1;
+      break;
+    case 'Difficult':
+      difficulty = 2;
+    default:
+      break;
+  }
+
+  console.log(difficulty);
+
+  var element = document.getElementById('welcome');
+  element.style.visibility = 'hidden';
+  element.parentNode.removeChild(element);
+  document.getElementById('background').style.visibility = 'visible';
+
+  gameLoop(difficulty);
+}
